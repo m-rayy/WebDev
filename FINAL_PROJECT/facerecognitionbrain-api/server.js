6f343e97b4d667666821eb31bcfe2a420b4b7ebb
m-rayy <egunes@gmail.com>
2020-11-09 07:56:38 +0100

    section26 signin & register WIP

--- FINAL_PROJECT/facerecognitionbrain-api/server.js
+++ FINAL_PROJECT/facerecognitionbrain-api/server.js
@@ -1,15 +1,5 @@
 const express = require('express');
 
-const app = express();
-
-app.get('/', (req, res)=> {
-    res.send('this is working');
-})
-
-app.listen(3000, ()=> {
-    console.log('app is running on port 3000');
-})
-
 /*
 API plan:
 / --> res = this is working
@@ -18,3 +8,41 @@ API plan:
 /profile/:userId --> GET = user
 /image --> PUT --> user (update rank)
 */
+
+const app = express();
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+// / --> res = this is working
+app.get('/', (req, res)=> {
+    res.json('this is working');
+})
+
+// /signin --> POST = success/fail (over HTTPS, passwords not shared with middleware)
+app.post('/signin', (req, res)=> {
+    // .json() has more features compared to .send()
+    res.send('signin is working')
+})
+
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000');
+})
